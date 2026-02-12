edd1784ba1bfb47c3d0501e68852a46800bddb4e
victormarand <marandvictor@gmail.com>
2026-02-12 18:09:30 +0100

    fix vm

--- app/home.tsx
+++ app/home.tsx
@@ -1,5 +1,4 @@
-import { useFocusEffect } from "@react-navigation/native";
-import { useLocalSearchParams, useRouter } from "expo-router";
+import { useLocalSearchParams, useRouter, useFocusEffect } from "expo-router";
 import { useCallback, useState } from "react";
 import { ImageBackground, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
